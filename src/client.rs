eea9a8814ca9bddbc5195c1c18c9bbd576553c23
Daniel Redly <dredly@gmail.com>
2023-07-05 22:19:42 +0100

    Changes to client

--- src/client.rs
+++ src/client.rs
@@ -8,8 +8,6 @@ pub struct Client {
     base_url: String,
 }
 
-// let body = reqwest::get(format!("http://localhost:3000/cities?point=POINT(-0.1276%2051.5074)&radius={setting.radius}&sort_by_random=true&minimum_population={setting.min_population}limit=2"))
-
 impl Default for Client {
     fn default() -> Self {
         Self {
@@ -34,12 +32,7 @@ impl Client {
         reqwest::get(URI).await.unwrap().json::<City>().await
     }
     pub async fn get_cities(&self, query: &CitiesQuery) -> Result<Vec<City>, reqwest::Error> {
-        let query_string = format!(
-            "cities?point={}&radius={}&sort_by_random=true&minimum_population={}&limit=2",
-            query.point.clone().unwrap(),
-            query.radius.unwrap(),
-            query.minimum_population.unwrap()
-        );
+        let query_string = make_query_string(query);
         let URI = self.get_full_URI(&query_string);
         // reqwest::blocking::get(URI)?.json::<Vec<City>>()
         reqwest::get(URI).await.unwrap().json::<Vec<City>>().await
@@ -56,16 +49,19 @@ impl Client {
     }
 }
 
-// server has to be running for these tests
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     #[test]
-//     fn rand() {
-//         let client = Client::default();
-//         let res = client.get_random_city();
-
-//         assert!(res.is_ok())
-
-//     }
-// }
+fn make_query_string(cities_query: &CitiesQuery) -> String {
+    let mut query_parts = vec![];
+    if let Some(country) = &cities_query.country {
+        query_parts.push(format!("country={}", country));
+    }
+    if let Some(sort_by_population) = &cities_query.sort_by_population {
+        query_parts.push(format!("sort_by_population={}", sort_by_population))
+    }
+    if let Some(limit) = &cities_query.limit {
+        query_parts.push(format!("limit={}", limit));
+    }
+    match query_parts.len() {
+        0 => "cities".to_string(),
+        _ => format!("cities?{}", query_parts.join("&"))
+    }
+}
