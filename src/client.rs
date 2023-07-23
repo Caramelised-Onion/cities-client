ad992f24af0309127ac4b7e6ecfba3891c6f5851
Daniel Redly <dredly@gmail.com>
2023-07-23 09:24:26 +0100

    Removed from-row feature, cleaned up some more query string stuff

--- src/client.rs
+++ src/client.rs
@@ -25,7 +25,6 @@ impl Client {
         }
     }
 
-    // TODO: make the query string only include the actual query string
     pub fn get_full_uri(&self, additional_path: &str) -> String {
         // TODO check for trailing or leading slash
         // or check if is there a nice inbuilt way to deal with this in one of deps
@@ -37,7 +36,6 @@ impl Client {
 impl Client {
     pub async fn get_random_city(&self) -> Result<City, reqwest::Error> {
         let uri = self.get_full_uri("rand");
-        // reqwest::get(uri).await.unwrap().json::<City>().await
         self.client
             .get(uri)
             .send()
@@ -46,6 +44,7 @@ impl Client {
             .json::<City>()
             .await
     }
+
     pub async fn get_cities(&self, query: &CitiesQuery) -> Result<Vec<City>, reqwest::Error> {
         let uri = self.get_full_uri("cities");
         self.client
@@ -70,14 +69,15 @@ impl Client {
             .await
     }
 
-    // TODO use a query from models instead
     pub async fn get_country_outline(
         &self,
         country_code: String,
     ) -> Result<String, reqwest::Error> {
-        let query_string = format!("countries?country_code={}", country_code);
-        let uri = self.get_full_uri(&query_string);
-        reqwest::get(uri)
+        let uri = self.get_full_uri("countries");
+        self.client
+            .get(uri)
+            .query(&[("country_code", country_code)])
+            .send()
             .await
             .unwrap()
             .json::<Country>()
